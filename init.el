4cba9a31fc7f54f1aa92c0126454007747e8b1c6
Edvin Syse <es@syse.no>
2023-10-30 08:50:18 +0100

    Disable menu-bar in GUI mode as well

--- init.el
+++ init.el
@@ -30,8 +30,7 @@
 (defun edmacs/init-new-frame (frame)
   (set-frame-size frame edmacs/frame-width edmacs/frame-height)
   (set-frame-font edmacs/frame-font nil t)
-  (if (not (window-system))
-	(menu-bar-mode -1)))
+  (menu-bar-mode -1))
 
 ;; Setup frame for initially loaded frame
 (edmacs/init-new-frame (selected-frame))
