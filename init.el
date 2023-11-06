4ee955e916a028486dfa058dee3b3f46cc2c69d9
Edvin Syse <es@cycle-start.no>
2023-11-06 23:16:59 +0100

    Activate surround.el

--- init.el
+++ init.el
@@ -325,11 +325,9 @@
 (add-to-list 'company-backends 'company-restclient)
 
 ;; Surround code with brackets and quotes
-;; (straight-use-package
-;;  '(surround :type git :host github :repo "edvin/surround.el")
-;;  :init (setq surround-shortcut "C-c C-s"))
-
-(surround-activate-keymap)
+(straight-use-package
+ '(surround :type git :host github :repo "edvin/surround.el"))
+(surround-mode 1)
 
 ;; Load user "settings" if exists
 (let ((edmacs/custom-init-file (concat user-emacs-directory "/user.el")))
