c68650de452b022d5a7c852848b5eb08cc835795
Edvin Syse <es@syse.no>
2023-12-13 15:39:59 +0100

    WIP

--- init.el
+++ init.el
@@ -176,10 +176,10 @@
 
 (use-package lsp-mode
   :hook (
-	 ;; (svelte-mode . lsp-deferred)
-	 ;; (typescript-mode . lsp-deferred)
-	 ;; (javascript-mode . lsp-deferred)
-	 ;; (go-mode . lsp-deferred)
+	 (svelte-mode . lsp-deferred)
+	 (typescript-mode . lsp-deferred)
+	 (javascript-mode . lsp-deferred)
+	 (go-mode . lsp-deferred)
 	 ;; (kotlin-mode . lsp-deferred)
      (lsp-mode . lsp-enable-which-key-integration))
   :init
@@ -191,20 +191,20 @@
 (use-package lsp-ui :commands lsp-ui-mode)
 
 ;; Prettier formatting - enable with (prettier-mode) when you need it
-;;(use-package prettier)
+(use-package prettier)
 
 ;;Svelte
-;; (use-package svelte-mode)
+(use-package svelte-mode)
 
 ;; TypeScript
-;; (use-package typescript-mode)
+(use-package typescript-mode)
 
 ;; TailwindCSS
-;; (use-package lsp-tailwindcss
-;;   :init
-;;   (setq lsp-tailwindcss-add-on-mode t)
-;;   :config
-;;   (setq lsp-tailwindcss-major-modes '(svelte-mode html-mode web-mode css-mode typescript-mode)))
+(use-package lsp-tailwindcss
+  :init
+  (setq lsp-tailwindcss-add-on-mode t)
+  :config
+  (setq lsp-tailwindcss-major-modes '(svelte-mode html-mode web-mode css-mode typescript-mode)))
 
 ;; Java LSP Server must use recent version of Java, but default projects to Java 8
 ;; (setenv "JAVA_HOME" "/usr/lib/jvm/java-17-jdk/")
@@ -281,8 +281,6 @@
 ;;   :init (setq markdown-command "multimarkdown")
 ;;   :bind (:map markdown-mode-map
 ;; 			  ("C-c C-e" . markdown-do)))
-
-(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
 
 ;; Enable vertico
 (use-package vertico
